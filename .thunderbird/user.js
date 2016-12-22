9b82f3e287ae1e6832f57a5949da3179df1fc0a1
Kirsi Rinnesalo <kirsi.rinnesalo@jyu.fi>
2016-12-22 10:39:52 +0200

    Add Thundebird user.js

---
+++ .thunderbird/user.js
@@ -0,0 +1,29 @@
+// Download mail from all accounts on startup
+user_pref("mail.check_all_imap_folders_for_new", true);
+
+// Change the reply header
+// 0 - No Reply-Text
+// 1 - "[Author] wrote:"
+// 2 - "On [date] [author] wrote:"
+// 3 - User-defined reply header. Use the prefs below in conjunction with this:
+user_pref("mailnews.reply_header_type", 2);
+
+// Date format
+// 0    No date (10:23 AM)
+// 1    Your system's long* date format (Friday, December 31 2003 10:23 AM)
+// 2    Your system's short* date format (12/31/1999 10:23 AM)
+// 3    Year and month, separated by a slash (1999/12 10:23 AM)
+// 4    Abbreviated day name (Fri 10:23 AM)
+user_pref("mail.ui.display.dateformat.default", 2);
+//user_pref("mail.ui.display.dateformat.thisweek", 4);
+user_pref("mail.ui.display.dateformat.today", 0);
+
+// Sorting mails: http://superuser.com/questions/13518/change-the-default-sorting-order-in-thunderbird
+// Sort order
+// 1 = Ascending
+// 2 = Descending
+user_pref("mailnews.default_sort_order", 1);
+// Sort type
+// 22 = Thread
+user_pref("mailnews.default_sort_type", 22);
+
--- README.md
+++ README.md
@@ -49,3 +49,21 @@ Basic ignore patterns for SVN, Eclipse, Netbeans, Idea, Emacs and so on...
 * Use `emacs` as default editor
 * Lots of extensions
 * Status aliases for queues
+
+## Thunderbird
+
+### Add-ons in use
+
+* **Colored Diffs:** Color diffs sent by VCS
+* **Extra Folder Column:** In core now
+* **Mail Redirect:** Allow redirect a.k.a. bounce
+* **Xpunge:** Button for emptying trash and junk and compacting folders
+
+### user.js
+
+* check all folders for new messages
+* reply header: "On [date] [author] wrote:"
+* short date format as default
+* no date format today
+* sort order ascending
+* sort by thread
