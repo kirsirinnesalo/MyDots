c885200a109bf95d14248ef4d5e0da7e57215d35
Kirsi Rinnesalo <kirkopo@gmail.com>
2019-10-28 09:37:55 +0200

    unicode settings: prefer utf-8 and acknowledge windows 10

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -35,6 +35,26 @@
 (setq split-height-threshold nil)
 (setq split-width-threshold 100)
 
+
+;;;;;;;;;;;;;;
+;; unicode
+
+(message "Setting UTF-8 encoding")
+
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(setq x-select-request-type '(UTF8_STRING COMPUND_TEXT TEXT STRING))
+(set-language-environment 'utf-8)
+(if (eq system-type 'windows-nt)
+    (progn
+      (set-clipboard-coding-system 'utf-16-le)
+      (set-selection-coding-system 'utf-16-le))
+  (set-selection-coding-system 'utf-8))
+
+
 ;;;;;;;;;;;;;;
 ;; theme
 
